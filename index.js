7809eaaf8bbbaf1f360d694fdaa011d97b6d041a
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-12-12 15:04:44 -0500

    Added route to update and route to delete product based on ID

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose"); // Import mongoose to connect to db
 const express = require("express"); // Import express and initialize app
 const app = express();
 app.use(express.json()); // Middleware to parse json
+app.use(express.urlencoded({ extended: false })); // Middleware for url-encoded form data
 
 // App listen for port number
 app.listen(PORT, () => {
@@ -21,7 +22,9 @@ mongoose
 // Import Database Models
 const Product = require("./models/product.model.js");
 
-// ----- ROUTES -----
+// ----- ROUTES ----- //
+
+// READ ROUTES
 app.get("/", (req, res) => {
 	res.send("Hello from Node API '/' Route");
 });
@@ -45,10 +48,44 @@ app.get("/api/product/:id", async (req, res) => {
 	}
 });
 
+// CREATE ROUTES
 app.post("/api/products", async (req, res) => {
 	try {
 		const product = await Product.create(req.body);
 		res.status(200).json(product);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+// UPDATE ROUTES
+app.put("/api/product/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const product = await Product.findByIdAndUpdate(id, req.body);
+
+		if (!product) {
+			return res.status(404).json({ message: "Product not found!" });
+		}
+
+		const updatedProduct = await Product.findById(id);
+		res.status(200).json(updatedProduct);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+// DELETE ROUTES
+app.delete("/api/product/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const product = await Product.findByIdAndDelete(id);
+
+		if (!product) {
+			return res.status(404).json({ message: "Product not found!" });
+		}
+
+		res.status(200).json({ message: "Product deleted successfully!" });
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
